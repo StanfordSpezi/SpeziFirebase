d25f54a4892af306d45078cb4ba6ce6e21372b82
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-02-15 22:36:05 -0800

    Initial Setup

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
 // swift-tools-version:5.7
 
 //
-// This source file is part of the TemplatePackage open source project
-// 
+// This source file is part of the CardinalKit open-source project
+//
 // SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
-// 
+//
 // SPDX-License-Identifier: MIT
 //
 
@@ -12,21 +12,62 @@ import PackageDescription
 
 
 let package = Package(
-    name: "TemplatePackage",
+    name: "CardinalKitFirebase",
+    defaultLocalization: "en",
     platforms: [
         .iOS(.v16)
     ],
     products: [
-        .library(name: "TemplatePackage", targets: ["TemplatePackage"])
+        .library(name: "CardinalKitFirebaseAccount", targets: ["CardinalKitFirebaseAccount"]),
+        .library(name: "CardinalKitFirebaseConfiguration", targets: ["CardinalKitFirebaseConfiguration"]),
+        .library(name: "CardinalKitFirestore", targets: ["CardinalKitFirestore"]),
+        .library(name: "CardinalKitFirestorePrefixUserIdAdapter", targets: ["CardinalKitFirestorePrefixUserIdAdapter"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/StanfordBDHG/CardinalKit", .upToNextMinor(from: "0.3.0")),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "10.3.0")
     ],
     targets: [
         .target(
-            name: "TemplatePackage"
+            name: "CardinalKitFirebaseAccount",
+            dependencies: [
+                .target(name: "CardinalKitFirebaseConfiguration"),
+                .product(name: "Account", package: "CardinalKit"),
+                .product(name: "CardinalKit", package: "CardinalKit"),
+                .product(name: "FirebaseAuth", package: "firebase-ios-sdk")
+            ]
+        ),
+        .target(
+            name: "CardinalKitFirebaseConfiguration",
+            dependencies: [
+                .product(name: "CardinalKit", package: "CardinalKit"),
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk")
+            ]
+        ),
+        .target(
+            name: "CardinalKitFirestore",
+            dependencies: [
+                .target(name: "CardinalKitFirebaseConfiguration"),
+                .product(name: "CardinalKit", package: "CardinalKit"),
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseFirestoreSwift", package: "firebase-ios-sdk")
+            ]
+        ),
+        .target(
+            name: "CardinalKitFirestorePrefixUserIdAdapter",
+            dependencies: [
+                .target(name: "CardinalKitFirestore"),
+                .product(name: "CardinalKit", package: "CardinalKit"),
+                .product(name: "FirebaseAuth", package: "firebase-ios-sdk")
+            ]
         ),
         .testTarget(
-            name: "TemplatePackageTests",
+            name: "CardinalKitFirebaseTests",
             dependencies: [
-                .target(name: "TemplatePackage")
+                .target(name: "CardinalKitFirebaseAccount"),
+                .target(name: "CardinalKitFirebaseConfiguration"),
+                .target(name: "CardinalKitFirestore"),
+                .target(name: "CardinalKitFirestorePrefixUserIdAdapter")
             ]
         )
     ]
