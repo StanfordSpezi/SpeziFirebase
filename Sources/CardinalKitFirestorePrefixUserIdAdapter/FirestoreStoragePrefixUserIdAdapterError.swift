4ebb44adabac669863d7262adf0a17de0be3933b
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-03-06 09:02:30 -0800

    CardinalKit Firebase (#1)

---
+++ Sources/CardinalKitFirestorePrefixUserIdAdapter/FirestoreStoragePrefixUserIdAdapterError.swift
@@ -0,0 +1,14 @@
+//
+// This source file is part of the CardinalKit open-source project
+//
+// SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+
+/// Indicates an error on the ``FirestorePrefixUserIdAdapter``.
+public enum FirestorePrefixUserIdAdapterError: Error {
+    /// The user is not yet signed in.
+    case userNotSignedIn
+}
---
+++ Tests/UITests/TestApp/Shared/TestAppDelegate.swift
@@ -0,0 +1,23 @@
+//
+// This source file is part of the CardinalKit open-source project
+//
+// SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+import CardinalKit
+import CardinalKitFirebaseAccount
+import CardinalKitFirestore
+import CardinalKitFirestorePrefixUserIdAdapter
+import SwiftUI
+
+
+class TestAppDelegate: CardinalKitAppDelegate {
+    override var configuration: Configuration {
+        Configuration(standard: TestAppStandard()) {
+            Firestore(settings: .emulator)
+            FirebaseAccountConfiguration(emulatorSettings: (host: "localhost", port: 9099))
+        }
+    }
+}
