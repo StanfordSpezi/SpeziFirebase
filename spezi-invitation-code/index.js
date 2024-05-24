99be22a015a6caae4e9ac22efdf93b47197539c9
Matthew Turk <mjturk@stanford.edu>
2024-05-24 16:44:26 -0700

    Remove extraneous lines

--- spezi-invitation-code/index.js
+++ spezi-invitation-code/index.js
@@ -11,19 +11,21 @@ const {https} = require("firebase-functions/v2");
 const {FieldValue} = require("firebase-admin/firestore");
 
 class InvitationCodeVerifier {
-  constructor(firestore) {
+  constructor(firestore, invitationCodePath = "invitationCodes", userPath = "users") {
     this.firestore = firestore;
+    this.invitationCodePath = invitationCodePath;
+    this.userPath = userPath;
   }
 
-  async verifyInvitationCode(invitationCode, userId) {
-    const invitationCodeRef = this.firestore.doc(`invitationCodes/${invitationCode}`);
+  async enrollUserInStudy(userId, invitationCode) {
+    const invitationCodeRef = this.firestore.doc(`${this.invitationCodePath}/${invitationCode}`);
     const invitationCodeDoc = await invitationCodeRef.get();
 
     if (!invitationCodeDoc.exists || invitationCodeDoc.data().used) {
       throw new https.HttpsError("not-found", "Invitation code not found or already used.");
     }
 
-    const userStudyRef = this.firestore.doc(`users/${userId}`);
+    const userStudyRef = this.firestore.doc(`${this.userPath}/${userId}`);
     const userStudyDoc = await userStudyRef.get();
 
     if (userStudyDoc.exists) {
@@ -44,23 +46,19 @@ class InvitationCodeVerifier {
   }
 
   async validateUserInvitationCode(userId) {
-    try {
-      const invitationQuerySnapshot = await this.firestore.collection("invitationCodes")
-        .where("usedBy", "==", userId)
-        .limit(1)
-        .get();
+    const invitationQuerySnapshot = await this.firestore.collection("invitationCodes")
+      .where("usedBy", "==", userId)
+      .limit(1)
+      .get();
 
-      if (invitationQuerySnapshot.empty) {
-        throw new https.HttpsError("not-found", `No valid invitation code found for user ${userId}.`);
-      }
+    if (invitationQuerySnapshot.empty) {
+      throw new https.HttpsError("not-found", `No valid invitation code found for user ${userId}.`);
+    }
 
-      const userDoc = await this.firestore.doc(`users/${userId}`).get();
+    const userDoc = await this.firestore.doc(`users/${userId}`).get();
 
-      if (!userDoc.exists || userDoc.data().invitationCode !== invitationQuerySnapshot.docs[0].id) {
-        throw new https.HttpsError("failed-precondition", "User document does not exist or contains incorrect invitation code.");
-      }
-    } catch (error) {
-      throw error;
+    if (!userDoc.exists || userDoc.data().invitationCode !== invitationQuerySnapshot.docs[0].id) {
+      throw new https.HttpsError("failed-precondition", "User document does not exist or contains incorrect invitation code.");
     }
   }
 }
