49fcceffe1c064d8ee61d8fdb8a85efc931ba434
Matthew Turk <mjturk@stanford.edu>
2024-06-15 13:35:02 -0700

    Require credentials

--- spezi-invitation-code/tests/invitationCodeVerifier.test.js
+++ spezi-invitation-code/tests/invitationCodeVerifier.test.js
@@ -17,7 +17,8 @@ describe("InvitationCodeVerifier", () => {
   let firestore;
 
   beforeAll(() => {
-    admin.initializeApp({credential: admin.credential.cert(require("/home/runner/work/_temp/google-application-credentials.json"))});
+    const credentials = JSON.parse(Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64, "base64").toString("utf8"));
+    admin.initializeApp({credential: admin.credential.cert(credentials)});
     verifier = new InvitationCodeVerifier();
     firestore = admin.firestore();
   });
