cc5329602c20ba605541fc7f44e9ae2868414141
Matthew Turk <mjturk@stanford.edu>
2024-06-15 12:56:10 -0700

    Next attempt

--- spezi-invitation-code/tests/invitationCodeVerifier.test.js
+++ spezi-invitation-code/tests/invitationCodeVerifier.test.js
@@ -17,8 +17,7 @@ describe("InvitationCodeVerifier", () => {
   let firestore;
 
   beforeAll(() => {
-    const credentials = JSON.parse(Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64, "base64").toString("utf8"));
-    admin.initializeApp({credential: admin.credential.cert(credentials)});
+    admin.initializeApp({credential: admin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64)});
     verifier = new InvitationCodeVerifier();
     firestore = admin.firestore();
   });
