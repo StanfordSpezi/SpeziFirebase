fa100b93daa24f26f8f179198a84585b4ff27054
Lukas Kollmer <hey@lukaskollmer.de>
2025-02-17 19:11:38 -0800

    x

--- Tests/UITests/TestAppUITests/FirestoreDataStorageTests.swift
+++ Tests/UITests/TestAppUITests/FirestoreDataStorageTests.swift
@@ -200,7 +200,6 @@ final class FirestoreDataStorageTests: XCTestCase {
 
         let contentFieldIdentifier = "Enter the element's optional content."
         try app.textFields[contentFieldIdentifier].delete(count: 100, options: .disableKeyboardDismiss)
-        //try app.textFields[contentFieldIdentifier].enter(value: content, options: .skipTextFieldSelection)
         app.textFields[contentFieldIdentifier].typeText(content)
     }
 }
